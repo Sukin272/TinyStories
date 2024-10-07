488c0439a38e0484243a81025f8e869975d3abbb
Sukin272 <anish.joishy@gmail.com>
2024-10-07 22:17:10 +0530

    interim sub

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # TinyStories
+
+## Files
+
+- data/tinystories.py: Creates a cache of tinystories dataset and saves it to cache_dir defined in config.py.
+
+- models/gpt2.py: Defines a Pytorch Lightning model based on gpt2 that will be trained from scratch.
+
+- scripts/evaluate_gpt2_pretrained.py: evaluates pretrained gpt2 model from huggingface on an LLM.
+
+- utils/llama_together_ai.py: evaluates a given story on Llama LLM using together.ai API.
+
+- utils/tokenizer.py: imports gpt2 tokenizer and adds required special tokens.
+
+## How to run
+
+- python3 -m scripts.evaluate_gpt2_pretrained evaluates the model defined in the said file.
+
+- python3 -m data.tinystories creates cache of the tinystories dataset
+
+- python3 -m models.gpt2 to check the model on an example input and example generation(gibberish for now).
---
+++ utils/config.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+from typing import Dict
+
+
+@dataclass
+class DataConfig:
+    name: str = "tinystories"
+    num_workers: int = 18
+    batch_size: int = 8
+    max_length: int = 512
+
+
+@dataclass
+class TokenizerConfig:
+    name: str = "gpt2"
+    cache_dir: str = (
+        ".cache/tokenizer"
+    )
+
+
+@dataclass
+class ModelConfig:
+    name: str = "gpt2"
+    gpt2: Dict[str, int] = field(
+        default_factory=lambda: {"hidden_size": 128, "layers": 2, "heads": 4}
+    )
+
+
+@dataclass
+class TrainingConfig:
+    epochs: int = 1
+    lr: float = 1e-4
+    warmup_steps: int = 1000
+    weight_decay: float = 0.01
+
+
+@dataclass
+class Config:
+    cache_dir: str = ".cache"
+    data: DataConfig = field(default_factory=DataConfig)
+    tokenizer: TokenizerConfig = field(default_factory=TokenizerConfig)
+    model: ModelConfig = field(default_factory=ModelConfig)
+    training: TrainingConfig = field(default_factory=TrainingConfig)
+
+
+config = Config()
